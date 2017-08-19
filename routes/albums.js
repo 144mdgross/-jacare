18864775b2c01a870a9e98c943ded4504bb0aa87
144mdgross <144.mdgross@gmail.com>
2017-08-19 10:36:45 -0600

    artists enpoint set up

--- routes/albums.js
+++ routes/albums.js
@@ -3,7 +3,30 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  // res.json({ albums })
+
+  res.json('{ get all }')
 });
+
+router.get('/:id', (req, res, next) => {
+  res.json('get one')
+})
+
+router.post('/', (req, res, next) => {
+  res.json('post')
+})
+
+router.patch('/', (req, res, next) => {
+  res.json('patch')
+})
+
+router.put('/', (req, res, next) => {
+  res.json('put')
+})
+
+router.delete('/', (req, res, next) => {
+  res.json('delete')
+})
+
+
 
 module.exports = router;
--- routes/artists.js
+++ routes/artists.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const knex = require('../knex')
 
-/* GET users listing. */
 router.get('/', (req, res, next) => {
-  // res.json({ artists })
+  knex('artists')
+    .orderBy('artist', 'asc')
+    .then(index => {
+      // envelope data for a layer of data security
+      let envelope = { index }
+      res.json(envelope)
+    })
 });
+
+router.get('/:id', (req, res, next) => {
+  knex('artists')
+    .join('albums_artists', 'artists.id', 'albums_artists.artist_id')
+    .join('albums', 'albums.id', 'albums_artists.album_id')
+    .select('artists.artist', 'albums.album')
+    .where('albums_artists.artist_id', req.params.id)
+    .then(data => {
+      console.log(data);
+      res.json(data)
+    })
+})
 
 module.exports = router;
